2c0ef62da28f6035558e794ba16eaea282c0ff12
MansiTripathi1522 <mansitripathi1522@gmail.com>
2022-02-07 21:32:51 +0530

    Java

---
+++ arr.java
@@ -0,0 +1,27 @@
+public class arr {
+    int n1,n2,n3,sum;
+    arr(int a,int b)
+    {
+n1=a;
+n2=b;
+    }
+    arr(int a,int b,int c)
+    {
+        n1=a;
+        n2=b;
+        n3=c;
+    }
+    void sum()
+    {
+        sum=n1+n2+n3;
+        System.out.println(sum);
+    }
+    public static void main()
+    {
+        arr obj= new arr(2,4);
+        obj.sum();
+        arr obj1=new arr(2,9,8);
+        obj1.sum();
+
+    }
+}
