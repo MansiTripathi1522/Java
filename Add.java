ae5781d21b37a243d30d9a7191f1652e7c55cd99
MansiTripathi1522 <mansitripathi1522@gmail.com>
2022-02-13 20:12:15 +0530

    commit

--- Add.java
+++ Add.java
@@ -2,13 +2,20 @@ import java.util.*;
 public class Add {
     public static void main(String[] args)
     {
-        int a=23,b=89;
+        double a=10;
+        int b=3;
        /* System.out.println("Enter any two Numbers");
      Scanner obj =new Scanner(System.in);
         
             a=obj.nextInt();
             b=obj.nextInt();
        */
+      
+    
         System.out.println("Sum ="+ (a+b));
+        System.out.println("Sub ="+ (a-b));
+        System.out.println("product ="+ (a*b));
+        System.out.println("mod ="+ (a%b));
+        System.out.println("divide ="+ (a/b));
     }
 }
