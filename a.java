7f5cff45273a0b753dcebc17a0e9f1b9b00c5a97
MansiTripathi1522 <mansitripathi1522@gmail.com>
2022-03-02 15:00:23 +0530

    commit

---
+++ a.java
@@ -0,0 +1,5 @@
+interface a {
+    void add(); //In Interface By default this method is abstract type 
+    //the class in which we will inherit our method we will define our method in that class only.
+
+}
